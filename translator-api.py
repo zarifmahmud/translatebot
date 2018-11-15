fbf13d26429eded4d4c115891fcd1fd025bc7a4e
Zarif Mahmud <zarif.mahmud@outlook.com>
2018-11-15 22:29:44 +0300

    Created first barebones translate function, which takes input text and language to translate to (detects input language), and outputs translation in console

--- translator-api.py
+++ translator-api.py
@@ -2,15 +2,34 @@ import requests
 from yandex_key import * #Contains my API key for Yandex translations.
 import urllib.parse
 
-yandexurl = "https://translate.yandex.net/api/v1.5/tr/translate?key={}&options=1".format(yandexkey)
+yandexurl = "https://translate.yandex.net/api/v1.5/tr/translate?key={}&options=1&".format(yandexkey)
+
 
 def translation_params(text: str, lang: str):
     """
     Need to add user given parameters to yandexurl, so this takes them in, and URL encodes them to make them URL
     friendly.
+
+    >>> translation_params("This is a test", "en-fr")
+    text=This+is+a+test&lang=en-fr
+
     """
     encode_dict = {"text": text, "lang": lang}
-    print(urllib.parse.urlencode(encode_dict))
+    return urllib.parse.urlencode(encode_dict)
 
-translation_params("This is a test", "French")
+# print(translation_params("This is a test", "en-fr"))
+
+def translate(text: str, lang: str) -> str:
+    """
+    The actual translation
+    """
+    urlext = translation_params(text, lang)
+    yan = yandexurl
+    yan += urlext
+    r = requests.get(yan)
+    textyan = r.text
+    print(textyan)
+
+translate("Cheese", "fr")
+
 
