3ef205fbb9d73bdfab757935106af787e690fd7f
Zarif Mahmud <zarif.mahmud@outlook.com>
2018-11-15 22:11:24 +0300

    First function in program, converts user translation parameters into something that can be appended to the API's URL

--- translator-api.py
+++ translator-api.py
@@ -1,3 +1,16 @@
 import requests
+from yandex_key import * #Contains my API key for Yandex translations.
+import urllib.parse
 
-r = requests.get()
+yandexurl = "https://translate.yandex.net/api/v1.5/tr/translate?key={}&options=1".format(yandexkey)
+
+def translation_params(text: str, lang: str):
+    """
+    Need to add user given parameters to yandexurl, so this takes them in, and URL encodes them to make them URL
+    friendly.
+    """
+    encode_dict = {"text": text, "lang": lang}
+    print(urllib.parse.urlencode(encode_dict))
+
+translation_params("This is a test", "French")
+
