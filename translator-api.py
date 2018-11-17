d4e54ae0ea2556c934531cb303315e6afd15c14b
Zarif Mahmud <zarif.mahmud@outlook.com>
2018-11-17 18:34:16 +0300

    Added function to convert shorthand language code to longhand, and updated main function to give more details in its output.

--- translator-api.py
+++ translator-api.py
@@ -9,7 +9,8 @@ import requests
 from yandex_key import *  # Contains my API key for Yandex translations.
 import urllib.parse
 
-yandexurl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key={}&options=1&".format(yandexkey)
+translateurl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key={}&options=1&".format(yandexkey)
+langlisturl = "https://translate.yandex.net/api/v1.5/tr.json/getLangs?key={}&ui=en".format(yandexkey)
 # We're using Yandex's option to return content in a JSON format rather than XML because the requests package
 # has a built in way of dealing with JSON.
 
@@ -25,19 +26,38 @@ def translation_params(text: str, lang: str):
     """
     encode_dict = {"text": text, "lang": lang}
     urlext = urllib.parse.urlencode(encode_dict)
-    return yandexurl + urlext
+    return translateurl + urlext
 
 # print(translation_params("This is a test", "en-fr"))
+
+
+def longhand(abbrev: str) -> str:
+    """
+    Uses Yandex's dictionary to convert the shorthand form of language that Yandex returns into it's full name
+
+    >>> longhand("en")
+    English
+    """
+
+    r = requests.get(langlisturl)
+    lang_dict = r.json()
+    return lang_dict['langs'][abbrev]
+
+# print(longhand('en'))
 
 
 def translate(text: str, lang: str) -> str:
     """
     The actual translation
     """
+
     yanurl = translation_params(text, lang)
     r = requests.get(yanurl)
     output_dict = r.json()  # Returns dictionary with translated text, and otheri info.
-    return output_dict["text"][0]
+    langdetected = longhand(output_dict["detected"]["lang"])
+    translation = output_dict["text"][0]
+    retstr = "Language Detected: {} \nTranslation: {} \nPowered by Yandex Translate".format(langdetected, translation)
+    return retstr
 
 #{'code': 200, 'detected': {'lang': 'en'}, 'lang': 'en-fr', 'text': ['Ceci est un test']}
 
