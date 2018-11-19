4fc5b226293c92c3a1475b177146c56fe1b381dd
Zarif Mahmud <zarif.mahmud@outlook.com>
2018-11-19 20:01:32 +0300

    Bot can now see and reply to mentions, using a stream generator of unread inbox messages

--- translator_bot.py
+++ translator_bot.py
@@ -1,5 +1,12 @@
 """
 Reddit bot for translation
+
+Note: Interesting error when trying to do
+if "translate-this" in unread.body and unread.is_root:
+Python parses it as if we're trying to find "translate-this" str both in unread, and unread.is_root,
+which obviously won't work, as unread.is_root is a bool. But this works
+if "translate-this" in unread.body and not unread.is_root:
+I guess the 'not' helps Python parse it better
 """
 import praw
 import schedule
@@ -11,4 +18,17 @@ import time
 reddit = praw.Reddit('bot1',
                      user_agent='Windows:translationbot:v0 (by /u/translate-this)')
 
-print(reddit.user.me())
+# for comment in reddit.subreddit('all').stream.comments():
+#     if "translate-this" in comment.lower():
+#         comment.reply("I see this")
+
+for unread in praw.models.util.stream_generator(reddit.inbox.unread):
+    # This creates a generator that continuously returns unread messages in my inbox.
+    print(unread)
+    print(unread.body)
+    if "translate-this" in unread.body and not unread.is_root:
+            unread.reply("You aren't a root")
+    else:
+        unread.reply("You are a root")
+
+
